0d9a389b4b3e25aff3a2e18beaa3ca7b865131b4
Леонид Вараксин <57279249+3arthWindAndFir3@users.noreply.github.com>
2021-03-09 13:07:37 +0500

    Файлы JS для 3 задания ЛР1

---
+++ webtechnologies/js-scripts/lr1_4/main.js
@@ -0,0 +1,26 @@
+var checkingInput=false;
+var password="пароль";
+while (checkingInput==false) {
+	var usersInput = prompt("Введите пароль для доступа к программе" 
+	+ " сравнения:");
+	if (usersInput!=password) {
+		alert ("Неверный пароль!");
+	}
+	else {
+		alert ("Добро пожаловать!"); 
+		checkingInput=true;
+	}
+};
+var x, y; 
+x=parseFloat(prompt("Введите значение х",''));
+// метод parseInt() переводит строку в целое число
+y=parseFloat(prompt("Введите значение у",'')); 
+if(x<y) {
+	alert("Число y больше x");
+} 
+else if (x>y) {
+	alert("Число x больше y"); 
+}
+else if (x==y) {
+	 alert ("x и y равны друг другу!");
+}
