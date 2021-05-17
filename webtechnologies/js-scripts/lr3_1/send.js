dd7648adb828219d60ea931f67596c9cb7620b79
Леонид Вараксин <57279249+3arthWindAndFir3@users.noreply.github.com>
2021-05-17 09:24:44 +0500

    Обновлен скрипт для lr3-1

--- webtechnologies/js-scripts/lr3_1/send.js
+++ webtechnologies/js-scripts/lr3_1/send.js
@@ -1,23 +1,13 @@
-var startArticles = 5;
-var numberOfArticles = (document.getElementsByTagName('article').length);
-console.log (numberOfArticles);
-var stringUrl = 'lr3_1.php?startArticles='+startArticles+'&numberOfArticles='+numberOfArticles+'';
-var rightUrl = '/lr3_1.php';
-var shortUrl='http://d996108w.beget.tech/'+stringUrl;
-
 $("#more-articles").click(function(){
+	var startArticles = 5;
+	var numberOfArticles = document.getElementsByTagName('article').length;
 	$.ajax({
-		crossDomain:true,
-		url: rightUrl,
 		method: 'get',
-		dataType: 'json',
-		data: {startArticles: 5, numberOfArticles: numberOfArticles}
+		url: "/3.php?startArticles=" + startArticles + "&numberOfArticles=" + numberOfArticles,
+       		dataType: "html",
+		success: function (data) {
+			alert ("Загружаю статьи, подождите..."); 
+			$("#put-here").html(data);
+			}
+		});
 	});
-	onclick=window.location.assign(shortUrl);
-}); 
-
-/*$("#more-articles").click(function(){
-	$.get('/get_number_articles.php', {startArticles: 5, currentArticles: numberOfArticles}, function(data){
-		alert(data);
-	});
-}); */
